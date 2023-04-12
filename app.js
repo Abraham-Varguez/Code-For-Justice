f12a7ba97d5a2c4f5c087eaa83e4442cc61dca4e
Juan Pardo <mrstudent.bear@gmail.com>
2023-04-12 19:49:36 -0400

    onclick

--- app.js
+++ app.js
@@ -7,5 +7,23 @@ const observer = new IntersectionObserver((entries) =>{
     })
 });
 
+let expanded = true;
+
 const hiddenElements = document.querySelectorAll('.hidden , .hidden-left, .hidden-right, .hidden-center, .hidden-center-1 , .hidden-center-2 , .hidden-center-3 , .hidden-center-4, .hidden-grow');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelector('.hamburger').addEventListener("click", myFunction);
+
+function myFunction() {
+    if (expanded){
+        document.getElementById("ham-menu").style.display = "none";
+        
+        expanded = false;
+    }
+    else{
+        document.getElementById("ham-menu").style.display = "flex";
+        expanded = true;
+    }
+    
+    
+} 
