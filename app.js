b2b2cfba33fce840413bd5f32644ce850113568e
Juan Pardo <mrstudent.bear@gmail.com>
2023-04-12 21:09:35 -0400

    navbar

--- app.js
+++ app.js
@@ -17,11 +17,16 @@ document.querySelector('.hamburger').addEventListener("click", myFunction);
 function myFunction() {
     if (expanded){
         document.getElementById("ham-menu").style.display = "none";
-        
+        document.getElementById("iconOpen").style.display = "flex";
+        document.getElementById("iconClosed").style.display = "none";
+   
         expanded = false;
     }
     else{
         document.getElementById("ham-menu").style.display = "flex";
+        document.getElementById("iconOpen").style.display = "none";
+        document.getElementById("iconClosed").style.display = "flex";
+        
         expanded = true;
     }
     
